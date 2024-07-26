4b94765757d435248dd3c57cd2f30eaaaab22175
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-26 12:02:38 +0200

    needs further testing

--- letter_changer.py
+++ letter_changer.py
@@ -1,14 +1,27 @@
+from math import e
 import unidecode
 import sys
 import os
+import chardet
+from pathlib import Path
 
 def convert_non_english_characters(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
+    '''Convert non-english characters to english characters'''
+    encoding = predict_encoding(file_path)
+    with open(file_path, 'r', encoding=encoding) as file:
         content = file.read()
     converted_content = unidecode.unidecode(content)
     
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, 'w', encoding=encoding) as file:
         file.write(converted_content)
+
+def predict_encoding(file_path: Path, n_lines: int=20) -> str:
+    '''Predict a file's encoding using chardet'''
+
+    with Path(file_path).open('rb') as f:
+        rawdata = b''.join([f.readline() for _ in range(n_lines)])
+
+    return chardet.detect(rawdata)['encoding']
 
 
 if __name__=='__main__':

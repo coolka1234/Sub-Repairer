7724607cf02c878e4fb3b4135153e4c634531ea4
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-07-26 10:37:26 +0200

    testing the script

--- letter_changer.py
+++ letter_changer.py
@@ -1,4 +1,6 @@
 import unidecode
+import sys
+import os
 
 def convert_non_english_characters(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -7,3 +9,19 @@ def convert_non_english_characters(file_path):
     
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(converted_content)
+
+
+if __name__=='__main__':
+    if len(sys.argv) < 2:
+        print('Please provide the file path as an argument')
+        sys.exit(1)
+    file_path = sys.argv[1]
+    if not os.path.exists(file_path):
+        print('File not found')
+        sys.exit(1)
+    convert_non_english_characters(file_path)
+    print('File converted successfully')
+
+
+
+
